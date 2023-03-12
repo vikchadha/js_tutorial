4b7fbf9897900d21550e1ac179215433e3d2cc3b
Vik Chadha <vikchadha@gmail.com>
2023-03-12 16:40:17 -0400

    Add a rudimentary palindrome detector

---
+++ day.js
@@ -0,0 +1,4 @@
+function dayName(date) {
+    const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday",    "Thursday", "Friday", "Saturday"];
+    return daysOfTheWeek[date.getDay()];
+    }
