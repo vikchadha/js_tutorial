97b44d482236f4f42a60db6b67c2088a66699c97
Vik Chadha <vikchadha@gmail.com>
2023-03-13 12:52:09 -0400

    Finish palindrome detector

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 let Phrase = require("vikchadha-palindrome");
 
-let string = prompt("Please enter a string for palindrome testing:")
-let phrase = new Phrase(string);
+function palindromeTester(event) {
+    event.preventDefault();
+    
+    let phrase = new Phrase(event.target.phrase.value);
+    let palindromeResult = document.querySelector("#palindromeResult");
 
-if (phrase.palindrome()) {
-    alert(`"${phrase.content}" is a palindrome!`);
-  } else {
-    alert(`"${phrase.content}" is not a palindrome.`)
-  }
+    if (phrase.palindrome()) {
+        palindromeResult.innerHTML = `"${phrase.content}" is a palindrome!`;
+    } else {
+        palindromeResult.innerHTML = `"${phrase.content}" is not a palindrome.`
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    let button = document.querySelector("#palindromeTester");
+    button.addEventListener("submit", function() {
+        palindromeTester(event);
+    });
+});
+
